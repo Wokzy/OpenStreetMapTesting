aa3d83276680bd55fce8824d171da7abb9aa2dcb
Wokzy <yegor6741@gmail.com>
2022-03-12 17:10:59 +0000

    XML and HTML testing

--- openstreetmap_test.py
+++ openstreetmap_test.py
@@ -21,7 +21,41 @@ def check(way, typ):
 		url = format_url(test['request_parameters'], test['test_parameters']['request_path'], typ)
 		text = make_request(url)
 
-		if json.loads(text) != test['expected_result']:
-			print(url)
-			assert False
-		
+		if typ == 'json':
+			
+			if json.loads(text) != test['expected_result']:
+				print(url)
+				assert False
+			
+		else:
+			if typ == 'xml':
+				txt1 = text.split('timestamp')[0]
+				txt2 = text.split('timestamp')[1].split('attribution')[1]
+				text = txt1 + 'attribution' + txt2
+
+			with open(test['expected_result'], 'r') as f:
+				res = f.read()
+				f.close()
+			
+			if text != res:
+				print(url)
+				assert False
+
+
+def test_rjson():
+	check('reverse', 'json')
+
+def test_djson():
+	check('direct', 'json')
+
+def test_dxml():
+	check('direct', 'xml')
+
+def test_rxml():
+	check('reverse', 'xml')
+
+def test_rhtml():
+	check('reverse', 'html')
+
+def test_dhtml():
+	check('direct', 'html')
